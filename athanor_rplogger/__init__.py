05bdce1596c90902bc71e1c3a5b325852bd47702
Andrew Bastien <volundmush@gmail.com>
2020-01-15 06:37:29 -0500

    Splitting Athanor into plugins and creating initial plugins!

--- athanor_rplogger/__init__.py
+++ athanor_rplogger/__init__.py
@@ -1,1 +1,8 @@
 INSTALLED_APPS = ["athanor_rplogger"]
+
+GLOBAL_SCRIPTS = dict()
+
+GLOBAL_SCRIPTS['roleplay'] = {
+    'typeclass': 'athanor_rplogger.controllers.AthanorRoleplayController',
+    'repeats': -1, 'interval': 50, 'desc': 'Event Controller for RP Logger System'
+}
